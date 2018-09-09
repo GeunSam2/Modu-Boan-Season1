51c89fc96fcb78347245b4290371c8e71c112d5f
clubmed2 <42707134+clubmed2@users.noreply.github.com>
2018-09-09 22:57:30 +0900

    Update 1.hi_modu.sh

--- Member_홍승현/shell_script/1.hi_modu.sh
+++ Member_홍승현/shell_script/1.hi_modu.sh
@@ -1,3 +1,12 @@
+1번. hi_modu.sh
+/root/moduboan/ 폴더아래에 자신의 영어이름폴더을 만드세요
+(해당 폴더가 없으면 만들어 주세요)
+ex)yeom_geun_cheol
+
+폴더 안에 readme.txt 파일을 만들고
+그 안에 'i love master yeom' 문장을 넣어주세요.
+
+
 #!/bin/bash
 
 mkdir -p  /root/moduboan/hong_seung_hyun
