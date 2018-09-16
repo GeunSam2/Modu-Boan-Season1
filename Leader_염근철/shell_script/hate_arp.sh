01263511cc94fc2454f9af5dcb1dfe3de4697645
yeomgeuncheol <41275199+yeomgeuncheol@users.noreply.github.com>
2018-09-17 00:38:18 +0900

    Update hate_arp.sh

--- Leader_염근철/shell_script/hate_arp.sh
+++ Leader_염근철/shell_script/hate_arp.sh
@@ -7,8 +7,8 @@ do
 	sleep 3
 	Swc=`expr $Swc \* -1`
 	arp -n | grep -v Address | awk '{print $1";"$3}'>${Tmp1}${Swc}
-	count1=`awk -F\; '{print $2}' ${Tmp1}${Swc} | sort | uniq -d`
-	if [ $count1 ]; then
+	hacker_mac=`awk -F\; '{print $2}' ${Tmp1}${Swc} | sort | uniq -d`
+	if [ $hacker_mac ]; then
 		wall "You Are UnderAttack!! (code:ARP)"
 		if [ $(grep $count1 -c ${Tmp1}`expr $Swc \* -1`) -ne 1 ]; then
 			wall "Can't find Attacker. But UnderAttack!"
